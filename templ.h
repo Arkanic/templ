687859739442f9d9b9892b36144c0ba27cd1313f
Arkanic <arkanic@users.noreply.github.com>
2021-12-26 09:04:28 +1300

    adjust

--- templ.h
+++ templ.h
@@ -1,9 +1,9 @@
-#define COMMAND "/opt/vc/bin/vcgencmd measure_temp"
-#define PREAMBLELEN 5
-
 // util.c
 float celtofahr(float celsius);
 
 // gettemp.c
+#define COMMAND "/opt/vc/bin/vcgencmd measure_temp"
+#define PREAMBLELEN 5
+
 float parsetemp(char output[16]);
 float gettemp(void);
