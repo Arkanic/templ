6cc6a5b43aa1eb95a4fc41754ffba1e08d29e692
Arkanic <arkanic@users.noreply.github.com>
2021-12-27 09:35:26 +1300

    proper args and option handling, pretty/standard mode:

--- templ.c
+++ templ.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include "templ.h"
+
+#define hasarg(arg) !strcmp(argv[j], arg)
+
+void showhelp(void) {
+	printf(
+"--celsius                Show temperature in celsius (default)\n"
+"--fahrenheit             Show temperature in fahrenheit\n"
+"--pretty                 Show pretty 16-seg numbers (default)\n"
+"--standard               Show simple text temperature\n"
+"--help                   Show this message\n"
+	);
+}
 
 volatile sig_atomic_t stop;
 
@@ -12,8 +25,23 @@ int main(int argc, char *argv[]) {
 	printf("temp logger\n");
 
 	unsigned short celsius = 1;
-	if(argc > 1) {
-		if(argv[1][0] == 'f') celsius = 0;
+	unsigned short pretty = 1;
+
+	for(int j = 1; j < argc; j++) {
+		int more = j + 1 < argc;
+
+		if(hasarg("--celsius")) celsius = 1;
+		else if(hasarg("--fahrenheit")) celsius = 0;
+		else if(hasarg("--pretty")) pretty = 1;
+		else if(hasarg("--standard")) pretty = 0;
+		else if(hasarg("--help")) {
+			showhelp();
+			exit(0);
+		} else {
+			fprintf(stderr, "Not enough args for option '%s'.\n\n", argv[j]);
+			showhelp();
+			exit(1);
+		}
 	}
 
 	printf("displaying temp in %s\n", celsius ? "celsius" : "fahrenheit");
@@ -24,10 +52,11 @@ int main(int argc, char *argv[]) {
 		float t = cputemp(0);
 		if(celsius == 0) t = celtofahr(t);
 
-		//printf("%.1f%c \n", t, celsius ? 'C' : 'F');
-		//printf("\x1b[1F");
-
-		prettyprint(t, celsius);
+		if(pretty) {
+			prettyprint(t, celsius);
+		} else {
+			printf("%.1f%c\n", t, celsius ? 'C' : 'F');
+		}
 
 		sleep(1);
 	}
